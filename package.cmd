a8f3169ce599a81c8a2ac0f5c5a9b2bfa6837921
HCLivess <admin@bismuth.cz>
2019-08-31 20:59:48 +0200

    remove functionalities handled by bismuthclient

--- package.cmd
+++ package.cmd
@@ -3,13 +3,7 @@ rmdir /s /q dist
 
 ROBOCOPY ../ bisbasic quantizer.py
 ROBOCOPY ../ bisbasic essentials.py
-ROBOCOPY ../ bisbasic connections.py
 ROBOCOPY ../ bisbasic options.py
-ROBOCOPY ../ bisbasic bisurl.py
-ROBOCOPY ../ bisbasic log.py
-ROBOCOPY ../ bisbasic quantizer.py
-ROBOCOPY ../ bisbasic simplecrypt.py
-
 
 python setup.py sdist bdist_wheel
 pause
